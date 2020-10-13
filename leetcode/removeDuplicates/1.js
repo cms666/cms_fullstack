de8babc8596d2fe2a610663e41cefac6d46439d1
cms666 <56785324+cms666@users.noreply.github.com>
2020-10-13 09:22:35 +0800

    原地删除有序数组重复项返回长度

---
+++ leetcode/removeDuplicates/1.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+  for(let i=0;i<nums.length;){
+    if(nums[i]==nums[i+1]){
+      nums.splice(i+1,1)
+    }
+    else{
+      i++
+    }
+  } 
+  return nums.length
+};
+console.log(removeDuplicates([1,1,2,2,3]))
