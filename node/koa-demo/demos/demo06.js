3da2f22c7bc26375f38d70f64181745df3f20121
cms666 <731814699@qq.com>
2020-10-29 18:14:32 +0800

    koa 路由前后端

---
+++ node/koa-demo/demos/demo06.js
@@ -0,0 +1,20 @@
+const Koa = require("koa");
+const router = require("koa-route");
+const app = new Koa();
+
+const about = (ctx) => {
+  ctx.response.type = "html";
+  ctx.response.body = '<a href="/">Index page</a>';
+};
+
+const main = (ctx) => {
+  ctx.response.body = "hello world";
+};
+
+// app.use(main);
+app.use(router.get("/about", about));
+app.use(router.get("/", main));
+
+app.listen(3000, () => {
+  console.log("3000端口已启动");
+});
