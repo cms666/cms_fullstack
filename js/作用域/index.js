86a45f869a75aebf5dea8a269d9554f5b6859ac2
cms666 <731814699@qq.com>
2020-10-30 17:01:02 +0800

    作用域

--- js/作用域/index.js
+++ js/作用域/index.js
@@ -1,5 +1,14 @@
-function foo(){
-  var a = 123
-  console.log(a);
+// function doSomething(a) {
+//   b = a + doSomethingElse(a * 2);
+//   console.log(b * 3);
+// }
+// function doSomethingElse(c) {
+//   return c - 1;
+// }
+// var b;
+// doSomething(2);
+var foo = true
+if(foo){
+  var a=2
 }
-foo()
+console.log(a);
