3435a27d0b9259a471ac1b4758060a2b817ecb5d
cms666 <731814699@qq.com>
2020-11-14 10:15:20 +0800

    node初学

---
+++ node/node-learn/index.js
@@ -0,0 +1,9 @@
+// console.log(Date);
+// console.log(requestAnimationfram);  //浏览器运行的下一帧，因为不是浏览器环境，所以不存在
+console.log(setImmediate);
+
+console.log(__filename);   //当前运行脚本所在位置
+console.log(__dirname);   //所在目录
+
+//进程对象
+console.log(process);
---
+++ node/node-learn/readme.md
@@ -0,0 +1,11 @@
+# node.js是一个基于chrome V8引擎的一个js运行环境
+# node.js 使用一个时间驱动，非阻塞式的I/O模型，轻量且高效
+
+
+# common.js    ECMAcomScript
+
+# npm
+
+# node.js 内置模块
+
+# 非阻塞式的I/O模型
