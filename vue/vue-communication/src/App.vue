253c66744acfa892f282da9f41698ed017a9a254
cms666 <731814699@qq.com>
2020-11-28 19:26:25 +0800

    vue-communication

---
+++ vue/vue-communication/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Hello :message="title" :fromHi="fromHi" @sendData="getData" />
+    {{ fromChild }}
+
+    <Hi @sendHi="getHi"/>
+    <A/>
+    <B/>
+  </div>
+</template>
+
+<script>
+import Hello from "@/components/Hello.vue";
+import Hi from "@/components/Hi.vue";
+import A from "@/components/A.vue";
+import B from "@/components/B.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "我爱我的祖国",
+      fromChild: "",
+      fromHi:''
+    };
+  },
+  components: {
+    Hello,
+    Hi,
+    A,
+    B
+  },
+  methods: {
+    getData(e) {
+      console.log(e);
+      this.fromChild = e;
+    },
+    getHi(e){
+      this.fromHi = e
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-communication/src/components/Hello.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="hello">
+      {{ message }}
+    </div>
+    <hr />
+    {{fromHi}}
+    <button @click="send">发送</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["message","fromHi"],
+  data() {
+    return {
+      msg: "我是子组件里面的数据",
+    };
+  },
+  methods: {
+    send() {
+      this.$emit("sendData", this.msg);
+    },
+  },
+};
+</script>
+
+<style>
+.hello {
+  color: red;
+  font-size: 20px;
+  width: 100px;
+  height: 50px;
+  border: 2px dotted blue;
+  margin: 0 auto;
+}
+</style>
