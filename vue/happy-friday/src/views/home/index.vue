511c57171e6e18c02c3766caa55fbf9f2c5d21da
cms666 <731814699@qq.com>
2020-12-10 09:32:23 +0800

    happyfriday

---
+++ vue/happy-friday/src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <itemContainer father-component="home"/>
+  </div>
+  
+</template>
+
+<script>
+import itemContainer from '../../components/itemContainer'
+export default {
+  components:{
+    itemContainer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/happy-friday/src/views/item/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <itemContainer father-component="item"/>
+  </div>
+  
+</template>
+
+<script>
+import itemContainer from '../../components/itemContainer'
+export default {
+  components:{
+    itemContainer
+  }
+}
+</script>
+
+<style>
+
+</style>
