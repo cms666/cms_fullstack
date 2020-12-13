b18afeda4b849ac2fcafa72b82c77ef8410185a9
cms666 <731814699@qq.com>
2020-12-13 16:13:13 +0800

    vuex

---
+++ vue/vuex/src/components/count.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="count">
+    <h3>{{$store.state.count}}</h3>
+    <h2>{{count}}</h2>
+    <h2>{{newCount}}</h2>
+    <!-- <h1>{{msg}}</h1> -->
+    <p>
+      <button @click="$store.commit('reduce')">-</button>
+      <button @click="add(3)">+</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapState,mapMutations,mapGetters,mapActions} from 'vuex'
+export default {
+  // computed:{
+  //   newCount(){
+  //     return this.$store.state.count*2
+  //   }
+  // },
+  data(){
+    return{
+      msg:''
+    }
+  },
+  // computed:mapState({
+  //   newCount:state=>state.count * 3
+  // }),
+  computed:{
+    ...mapState(['count']),
+    ...mapGetters(['newCount'])
+  },
+  // mounted(){
+  //   this.msg = [...mapState].count
+  // },
+  methods:{
+    ...mapMutations(['add','reduce']),
+    // add(){
+    //   this.$store.state.count++
+    // }
+    ...mapActions(['addAction'])
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vuex/src/vuex/store.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const state={
+  count:1
+}
+const mutations={
+  add(state,num){
+    setTimeout(() => {
+      state.count+=num
+    }, 3000);
+    console.log('我后出现');
+  },
+  reduce(state){
+    state.count--
+  }
+}
+
+const getters = {
+  newCount:(state)=>{
+    return state.count**2
+  }
+}
+const actions ={
+  addAction(context,n){
+    setTimeout(() => {
+      context.commit('add',n)
+    }, 3000);
+    console.log('我先出现');
+  }
+}
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters,
+  actions
+})
