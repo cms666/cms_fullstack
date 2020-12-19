ba6d33f6cc21ebfa9b69173a947b668739eb27c7
cms666 <731814699@qq.com>
2020-12-19 16:43:17 +0800

    深浅拷贝

---
+++ 手写代码/深浅拷贝/index.js
@@ -0,0 +1,14 @@
+/* let arr = ['old',1,true,null,undefined]
+let new_arr = arr.concat()
+arr[0]= 'new'
+console.log(arr);
+console.log(new_arr); */
+
+/* let arr = ['old',1,true,null,undefined]
+let new_arr = arr.slice()
+console.log(new_arr); */
+
+/* let arr = [{name:'old'},1,true,null,undefined]
+let new_arr = JSON.parse(JSON.stringify(arr))
+arr[0].name = 1
+console.log(new_arr); */
