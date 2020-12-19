a10ba9233a7c01f22540062357e933ebeb11ac9b
cms666 <731814699@qq.com>
2020-12-19 16:42:11 +0800

    数组扁平

---
+++ 手写代码/数组扁平/index.js
@@ -0,0 +1,29 @@
+let arr = [1, [1, [1, 2]]];
+/* let res = [];
+function falten(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      falten(arr[i]);
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
+console.log(falten(arr)); */
+
+// console.log(arr.toString());
+
+//reduce
+/* function falten(arr){
+  return arr.reduce(function(pre,next){
+    return pre.concat(Array.isArray(next) ? falten(next):next)
+  },[])
+} */
+function falten(arr){
+  while(arr.some(item => Array.isArray(item))){
+    arr = [].concat(...arr)
+  }
+  return arr
+}
+console.log([].concat(...arr));
