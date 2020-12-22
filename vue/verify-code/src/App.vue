2fc3fef4d65de57ac838319ce4978ec8b29be2ef
cms666 <731814699@qq.com>
2020-12-22 16:51:29 +0800

    发布至npm仓库

---
+++ vue/verify-code/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <verifyCode/>
+  </div>
+</template>
+
+<script>
+import verifyCode from "./verify-code"
+export default {
+  components:{
+    verifyCode
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/verify-code/src/main.js
@@ -0,0 +1,3 @@
+import VerifyCode from './verify-code.vue'
+VerifyCode.install = Vue => Vue.compotent(VerifyCode.name, VerifyCode)
+export default VerifyCode
