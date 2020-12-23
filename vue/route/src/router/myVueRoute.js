7936ff6e262f602d01c29cee7368704d0a17f575
cms666 <731814699@qq.com>
2020-12-23 16:36:53 +0800

    更新

--- vue/route/src/router/myVueRoute.js
+++ vue/route/src/router/myVueRoute.js
@@ -1,30 +1,86 @@
-class VueRouter{
-
+let Vue = null;
+class HistoryRoute {
+  constructor () {
+    this.current = null
+  }
 }
+class VueRouter {
+  constructor (options) {
+    this.mode = options.mode || 'hash'
+    this.routes = options.routes || []
+    this.routesMap = this.createMap(this.routes)
 
-VueRouter.install = function () {
+    this.history = new HistoryRoute() // 装当前路由的状态
+    this.init()
+  }
+  // 初始化
+  init() {
+    if (this.mode === 'hash') {
+      location.hash ? '' : location.hash = '/';
+      window.addEventListener('load', () => {
+        this.history.current = location.hash.slice(1)
+      })
+      window.addEventListener('hashchange', () => {
+        this.history.current = location.hash.slice(1)
+      })
+    } else {
+      location.pathname ? '' : location.pathname = '/';
+      window.addEventListener('load', () => {
+        this.history.current = location.pathname
+      })
+      window.addEventListener('popstate', () => {
+        this.history.current = location.pathname
+      })
+    }
+  }
+  createMap(routes) {
+    return routes.reduce((pre, current) => {
+      pre[current.path] = current.component  // { '/home': Home }
+      return pre
+    }, {})
+  }
+}
+VueRouter.install = function(v) {
+  Vue = v;
+  Vue.mixin({
+    beforeCreate() {
+      if (this.$options && this.$options.router) { // 如果是根组件
+        this._root = this
+        this._router = this.$options.router
 
+        Vue.util.defineReactive(this, 'xxx', this._router.history)
+      } else { // 如果是子组件
+        this._root = this.$parent && this.$parent._root
+      }
+      Object.defineProperty(this, '$router', {
+        get () {
+          return this._root._router
+        }
+      })
+      Object.defineProperty(this, '$route', {
+        get () {
+          return this._root._router.history.current
+        }
+      })
+    }
+  })
+  Vue.component('router-link', {
+    props:{
+      to:String
+    },
+    render(h) {
+      let mode = this._self._root._router.mode
+      let to = mode === 'hash' ? '#' + this.to : this.to
+      return h('a', {attrs:{href: to}}, this.$slots.default)
+    }
+  })
+  Vue.component('router-view', {
+    render(h) {
+      let current = this._self._root._router.history.current
+      let routeMap = this._self._root._router.routesMap
+      return h(routeMap[current])
+    }
+  })
 }
 
 export default VueRouter
-
-class Vue{
-  constructor(){
-    _installedPlogins: ''
-  }
-}
-Vue.use = function (plogin){
-  const installedPlogins = (this._installedPlogins || (this._installedPlogins =[]))
-  if(installedPlogins.indexOf(plogin) > 1){
-    return this
-  }
-  const args = toArray(arguments, 1)
-  args.unshift(this)
-  if(typeof plogin.install === 'function'){
-    plogin.install.apply(plogin, args)
-  }else if(typeof plogin === 'function'){
-    plogin.apply(null, plogin, args)
-  }
-  installedPlogins.push(plogin)
-  return this
-}
