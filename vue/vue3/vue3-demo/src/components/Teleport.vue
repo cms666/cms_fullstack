78bfb8ab87de78782e21021f7847c6b5c3624b21
cms666 <731814699@qq.com>
2021-01-04 17:28:54 +0800

    vue2

---
+++ vue/vue3/vue3-demo/src/components/Teleport.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="confirm-model">
+    <button>打开</button>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup(){
+    let isOpen = ref(false)
+    return {isOpen}
+  }
+}
+</script>
+
+<style>
+
+</style>
