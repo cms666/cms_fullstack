b55555339ca6520a4ccaf75b0c222c8b4c97fcbb
cms666 <731814699@qq.com>
2021-01-08 13:42:34 +0800

    async-await

---
+++ interview/async-await/2.js
@@ -0,0 +1,13 @@
+function getJson() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      console.log(2);
+      resolve(2)
+    }, 2000)
+  })
+}
+async function testAsync() {
+  await getJson()
+  console.log(3);
+}
+testAsync()
