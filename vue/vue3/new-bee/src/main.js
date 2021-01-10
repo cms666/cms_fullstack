19b7a13fc353ef6e955bb84c4ecb75c0e9e201d5
cms666 <731814699@qq.com>
2021-01-10 11:53:29 +0800

    new-bee

--- vue/vue3/new-bee/src/main.js
+++ vue/vue3/new-bee/src/main.js
@@ -1,10 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './common/style/base.less'
-import Vant from 'vant';
-import 'vant/lib/index.css';
-import 'lib-flexible/flexible'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./common/style/base.less";
+import Vant from "vant";
+import "vant/lib/index.css";
+import "lib-flexible/flexible";
 
-createApp(App).use(store).use(router).use(Vant).mount('#app')
+const app = createApp(App);
+//全局过滤器
+app.config.globalProperties.$filters = {
+  prefix(url) {
+    if (url && url.startsWith("http")) {
+      return url;
+    } else {
+      url = `http://47.99.134.126:28019${url}`;
+      return url;
+    }
+  },
+};
+
+app
+  .use(store)
+  .use(router)
+  .use(Vant)
+  .mount("#app");
