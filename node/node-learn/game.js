f0b38aa73b7c3be7c2aad11b75d983ce41866db2
cms666 <731814699@qq.com>
2021-01-21 18:06:54 +0800

    更新

--- node/node-learn/game.js
+++ node/node-learn/game.js
@@ -2,9 +2,12 @@
 
 // console.log(process.argv);
 
-var playerAction = process.argv[process.argv.length - 1];
+// var playerAction = process.argv[process.argv.length - 1];
 // console.log(playerAction);
 
+
+
+module.exports = function(playerAction){
 //电脑随机生成三个
 var random = Math.random() * 3;
 if (random < 1) {
@@ -17,13 +20,17 @@ if (random < 1) {
 
 //进行比较
 if (playerAction === computerAction) {
-  console.log("平局");
+  return 0
+  // console.log("平局");
 } else if (
   (computerAction === "rock" && playerAction === "paper") ||
   (computerAction === "scissor" && playerAction === "rock") ||
   (computerAction === "paper" && playerAction === "scissor")
 ) {
-  console.log('你赢了');
+  return 1
+  // console.log('你赢了');
 }else{
-  console.log('你输了');
+  return 2
+  // console.log('你输了');
+}
 }
