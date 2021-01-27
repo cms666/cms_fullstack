e74a301974b56a59e5230b394506e056361bf4f0
cms666 <731814699@qq.com>
2021-01-27 14:15:34 +0800

    面试相关

---
+++ interview/大文件上传/server/index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const server = http.createServer()
+
+server.on('request', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  if (req.method === "OPTIONs") {
+    res.status = 200
+    res.end()
+    return
+  }
+})
+
+// 接收切片
+// 合成切片
+
+
+server.listen(3000, () => {
+  console.log('3000端口已经启动');
+})
