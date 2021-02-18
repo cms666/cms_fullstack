3b31532564b7d09931dfdae855cff56009586ded
cms666 <731814699@qq.com>
2021-02-18 21:35:44 +0800

    股票

---
+++ leetcode/maxProfit/1.js
@@ -0,0 +1,17 @@
+/* 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+
+注意：你不能在买入股票前卖出股票。 */
+var maxProfit = function (prices) {
+  let max = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[j] - prices[i] > max) {
+        max = prices[j] - prices[i];
+      }
+    }
+  }
+  return max;
+};
+console.log(maxProfit([7, 6, 4, 3, 1]));
