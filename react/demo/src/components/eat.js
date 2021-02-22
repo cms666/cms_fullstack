243703004f8b6540c86007b3c7016579a6d64fc3
cms666 <731814699@qq.com>
2021-02-22 16:27:14 +0800

    初学

--- react/demo/src/components/eat.js
+++ react/demo/src/components/eat.js
@@ -1,34 +1,67 @@
-import react, {Component, Fragment} from 'react'
-
-class Eat extends Component{
-  constructor(props){
-    super(props)
+import react, { Component, Fragment } from "react";
+import "../style/eat.css";
+import PropTypes from 'prop-types'
+import EatItem from "./eatItem";
+class Eat extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      inputValue:'hello',
-      list:[],
-    }
+      inputValue: "hello",
+      list: ["宫保鸡丁", "肥肠"],
+    };
   }
 
-  inputChange(e){
+  inputChange(e) {
     // console.log(e.target.value);
     // this.state.inputValue = e.target.value
     this.setState({
-      inputValue:e.target.value
-    })
+      inputValue: e.target.value,
+    });
   }
-  render(){
+  addList() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+    });
+  }
+  deleteItem(index) {
+    let list = this.state.list;
+    list.splice(index, 1);
+    this.setState({
+      list,
+    });
+  }
+  render() {
     return (
       <Fragment>
         <div>
-          <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}></input>
-        <button>下单</button></div>
+          {/* <label for="addGoods">加菜</label> */}
+          <input
+            id="addGoods"
+            className="input"
+            value={this.state.inputValue}
+            onChange={this.inputChange.bind(this)}
+          ></input>
+          <button onClick={this.addList.bind(this)}>下单</button>
+        </div>
         <ul>
-          <li>宫保鸡丁</li>
-          <li>肥肠</li>
+          {this.state.list.map((item, index) => {
+            {
+              /*return <li key={index+item} onClick={this.deleteItem.bind(this,index)}>{item}</li>*/
+            }
+            return (
+              <EatItem
+                content={item}
+                deleteItem={this.deleteItem.bind(this)}
+                index={index}
+                key={index + item}
+                list={this.state.list}
+              />
+            );
+          })}
         </ul>
       </Fragment>
-    )
+    );
   }
 }
 
-export default Eat
+export default Eat;
---
+++ react/demo/src/components/eatItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+class EatItem extends Component {
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  state = {  }
+  handleClick(){
+    // console.log(this.props.index);
+    this.props.deleteItem(this.props.index)
+    this.props.list = []
+  }
+  render() { 
+    return ( 
+      <li onClick={this.handleClick}>{this.props.content}</li>
+     );
+  }
+}
+ 
+export default EatItem;
